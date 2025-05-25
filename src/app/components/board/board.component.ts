34e324fa543843c0e0076c8335ec45fe65230592
Macedman <ecmaceda17@gmail.com>
2025-05-25 15:10:27 +0800

    initial build of drag and drop

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ListComponent } from '../list/list.component';
 
 @Component({
   selector: 'app-board',
-  imports: [],
+  imports: [ListComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
