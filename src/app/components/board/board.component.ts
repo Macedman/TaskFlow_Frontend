b3487895906fb287e84496a40e9cad47d1d1a6e3
Macedman <ecmaceda17@gmail.com>
2025-05-26 15:30:15 +0800

    added tailwindcss for styling and styled login page

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ListComponent } from '../list/list.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-board',
-  imports: [ListComponent],
+  imports: [ListComponent, NavbarComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
