86ed891f295aa9cde600ca5eb6abd40dd12c77aa
Macedman <ecmaceda17@gmail.com>
2025-07-23 00:26:43 +0800

    Enabled close when mouseclick outside the div and escape key for Add card and add list

--- src/app/components/add-card/add-card.component.ts
+++ src/app/components/add-card/add-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, viewChild, OnInit, HostListener } from '@angular/core';
 import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -7,26 +7,49 @@ import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './add-card.component.html',
   styleUrl: './add-card.component.css'
 })
-export class AddCardComponent {
+export class AddCardComponent implements OnInit {
 
   isAddingCard = signal(false);
   newCard = new FormControl('', Validators.required);
+  readonly myAddCardTextArea = viewChild<ElementRef<HTMLTextAreaElement>>('myAddCardTextArea');
+  readonly myAddCardDiv = viewChild<ElementRef<HTMLDivElement>>('myAddCardDiv');
 
+
+  ngOnInit(): void {
+
+  }
+
+  //listen on outside click
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const clickedInside = this.myAddCardDiv()?.nativeElement.contains(event.target as Node);
+    if (!clickedInside) {
+      this.isAddingCard.set(false);
+    }
+  }
+
+  //listen for ESC click
+  @HostListener('document:keydown.escape', ['event'])
+  onEscapePress(event: KeyboardEvent) {
+    this.isAddingCard.set(false);
+  }
 
    addCard() {
     this.isAddingCard.set(true);
-    console.log(this.isAddingCard());
+    setTimeout(() => {
+      if (this.myAddCardTextArea()) {
+        this.myAddCardTextArea()?.nativeElement.focus();
+      }
+    });
   }
 
   cancelAddCard() {
     this.isAddingCard.set(false);
     this.newCard.reset();
-    console.log(this.isAddingCard());
   }
 
   confirmAddCard() {
-    console.log(this.isAddingCard());
-    console.log(this.newCard.value);
+    //close add card when invalid
     if (this.newCard.invalid) {
       this.isAddingCard.set(false);
     }
--- src/app/components/add-list/add-list.component.ts
+++ src/app/components/add-list/add-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, signal, viewChild, ViewChild } from '@angular/core';
+import { Component, ElementRef, signal, viewChild, OnInit, HostListener, ViewChild } from '@angular/core';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
@@ -7,15 +7,35 @@ import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
   templateUrl: './add-list.component.html',
   styleUrl: './add-list.component.css'
 })
-export class AddListComponent {
+export class AddListComponent implements OnInit {
   isAdding = signal(false);
 
   newList = new FormControl('', Validators.required);
   //this textarea is conditionally rendered, used viewchild to make sure it is available for focus
   //the purpose of this viewchild is to get the textarea element and focus on it when Add list button is clicked
   readonly textarea = viewChild<ElementRef<HTMLTextAreaElement>>('myTextArea');
+  readonly myAddListDiv = viewChild<ElementRef<HTMLDivElement>>('myAddListDiv');
 
   constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
+  //listen to outside click
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const clickedInside = this.myAddListDiv()?.nativeElement.contains(event.target as Node);
+    if (!clickedInside) {
+      this.isAdding.set(false);
+    }
+  }
+
+    //listen for ESC click
+  @HostListener('document:keydown.escape', ['$event'])
+  onEscapePress(event: KeyboardEvent) {
+    this.isAdding.set(false);
+  }
 
 
   addList() {
